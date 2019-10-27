0d48c029d5c0691b83da8bc46d516cf8d1204c6a
Akshay Kumar <akshay2796@gmail.com>
2019-10-28 02:51:59 +0530

    Updates

---
+++ precache-manifest.8c2ecc67d15bc0e01495348a565b88a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c38ab355c0c7896dcc00129771b6ff3",
+    "url": "/Mapbox-ReactJS-API/index.html"
+  },
+  {
+    "revision": "df13ab78f89fc04b5c2e",
+    "url": "/Mapbox-ReactJS-API/static/css/2.544a67c8.chunk.css"
+  },
+  {
+    "revision": "b27400da905628b77f75",
+    "url": "/Mapbox-ReactJS-API/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "df13ab78f89fc04b5c2e",
+    "url": "/Mapbox-ReactJS-API/static/js/2.8e7c0557.chunk.js"
+  },
+  {
+    "revision": "b27400da905628b77f75",
+    "url": "/Mapbox-ReactJS-API/static/js/main.ee28dda0.chunk.js"
+  },
+  {
+    "revision": "d0f60fe5f6dc1ee918ea",
+    "url": "/Mapbox-ReactJS-API/static/js/runtime-main.ffcf74bb.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Mapbox-ReactJS-API/precache-manifest.8c2ecc67d15bc0e01495348a565b88a5.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Mapbox-ReactJS-API/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
